04a9e10823cf69a7ef65b92d79ae24935d0f0ab0
Yitzack <isarver@physics.tamu.edu>
2023-10-01 17:15:49 -0600

    Table of ratios of N-sphere/N-cube random darts and how they relate to pi

--- Pi_Monte_Carlo/Pi.cpp
+++ Pi_Monte_Carlo/Pi.cpp
@@ -2,4 +2,19 @@
 #include<cmath>
 using namespace std;
 
+/*
+N	V_sphere=XR^N	V_cube=XR^N	pi=Number*Ratio^(1/n)
+2	pi		4		4*circle/total
+3	4/3pi		8		6*circle/total
+4	1/2pi^2	16		4*sqrt(2*circle/total)
+5	8/15pi^2	32		2*sqrt(15*circle/total)
+6	1/6pi^3	64		4*(6*circle/total)^(1/3)
+7	16/105pi^3	128		2*(105*circle/total)^(1/3)
+8	1/24pi^4	256		4*(24*circle/total)^(1/4)
+9	32/945pi^4	512		2*(945*circle/total)^(1/4)
+10	1/120pi^5	1024		4*(120*circle/total)^(1/5)
+*/
 
+int main()
+{
+}
