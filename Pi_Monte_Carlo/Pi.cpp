97dac2c4d7129a53456fb512105cd92114475f01
Yitzack <isarver@physics.tamu.edu>
2023-10-02 12:19:32 -0600

    Changing varibles out for larger integers and more accurate floating points. Eliminates the chance of bad stddev calculations.

--- Pi_Monte_Carlo/Pi.cpp
+++ Pi_Monte_Carlo/Pi.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<iomanip>
 #include<cmath>
 using namespace std;
 
@@ -15,10 +16,26 @@ N	V_sphere=XR^N	V_cube=XR^N	pi=Number*Ratio^(1/n)
 10	1/120pi^5	1024		4*(120*circle/total)^(1/5)
 */
 
-int Samples_in_Sphere(float Sample[10], int N)
+double mean(double array[], int N)
+{
+	double answer = 0;
+	for(int i = 0; i < N; i++)
+		answer += array[i];
+	return(answer/double(N));
+}
+
+double stddev(double array[], int N)
+{
+	double answer = 0;
+	for(int i = 0; i < N; i++)
+		answer += pow(array[i],2);
+	return(sqrt(answer/double(N)-pow(mean(array, N),2)));
+}
+
+int Samples_in_Sphere(double Sample[10], int N)
 {
 	int In = 0;
-	float distance;
+	double distance;
 
 	for(int i = 0; i < 10; i += N)
 	{
@@ -34,17 +51,19 @@ int Samples_in_Sphere(float Sample[10], int N)
 
 int main()
 {
-	int Samples_Used[9] = {0,0,0,0,0,0,0,0,0};
-	int Points_in_Sphere[9] = {0,0,0,0,0,0,0,0,0};
+	unsigned long long int Samples_Used[9] = {0,0,0,0,0,0,0,0,0};
+	unsigned long long int Points_in_Sphere[9] = {0,0,0,0,0,0,0,0,0};
 	int Sub_Samples[9] = {5,3,2,2,1,1,1,1,1};
-	float Sample[10];
+	double Sample[10];
+	double pi[9];
+	double Mean, StdDev;
 
 	srand(time(NULL));
 
 	do
 	{
 		for(int i = 0; i < 10; i++)
-			Sample[i] = 2.*float(rand())/float(RAND_MAX)-1.;	//Uniform random between -1 and 1, not recommended RGN method
+			Sample[i] = 2.*double(rand())/double(RAND_MAX)-1.;	//Uniform random between -1 and 1, not recommended RGN method
 
 		for(int N = 2; N <= 10; N++)
 		{
@@ -52,9 +71,29 @@ int main()
 			Samples_Used[N-2] += Sub_Samples[N-2];
 		}
 
-		if(Samples_Used[8]%1000==999)
-			cout << Samples_Used[8]+1 << " " << 4.*float(Points_in_Sphere[0])/float(Samples_Used[0]) << " " << 6.*float(Points_in_Sphere[1])/float(Samples_Used[1]) << " " << 4.*sqrt(2.*float(Points_in_Sphere[2])/float(Samples_Used[2])) << " " << 2.*sqrt(15.*float(Points_in_Sphere[3])/float(Samples_Used[3])) << " " << 4.*pow(6.*float(Points_in_Sphere[4])/float(Samples_Used[4]),1./3.) << " " << 2.*pow(105.*float(Points_in_Sphere[5])/float(Samples_Used[5]),1./3.) << " " << 4.*pow(24.*float(Points_in_Sphere[6])/float(Samples_Used[6]),.25) << " " << 2.*pow(945.*float(Points_in_Sphere[7])/float(Samples_Used[7]),.25) << " " << 4.*pow(120.*float(Points_in_Sphere[8])/float(Samples_Used[8]),.2) << endl;
-	}while(Samples_Used[8] < 100000);
+		if(Samples_Used[8]%1000000==999999)
+		{
+			pi[0] = 4.*double(Points_in_Sphere[0])/double(Samples_Used[0]);
+			pi[1] = 6.*double(Points_in_Sphere[1])/double(Samples_Used[1]);
+			pi[2] = 4.*sqrt(2.*double(Points_in_Sphere[2])/double(Samples_Used[2]));
+			pi[3] = 2.*sqrt(15.*double(Points_in_Sphere[3])/double(Samples_Used[3]));
+			pi[4] = 4.*pow(6.*double(Points_in_Sphere[4])/double(Samples_Used[4]),1./3.);
+			pi[5] = 2.*pow(105.*double(Points_in_Sphere[5])/double(Samples_Used[5]),1./3.);
+			pi[6] = 4.*pow(24.*double(Points_in_Sphere[6])/double(Samples_Used[6]),.25);
+			pi[7] = 2.*pow(945.*double(Points_in_Sphere[7])/double(Samples_Used[7]),.25);
+			pi[8] = 4.*pow(120.*double(Points_in_Sphere[8])/double(Samples_Used[8]),.2);
+			Mean = mean(pi, 9);
+			StdDev = stddev(pi, 9);
+
+			cout << Samples_Used[8]+1 << setw(8) << pi[0] << setw(8) << pi[1] << setw(8) << pi[2] << setw(8) << pi[3] << setw(8) << pi[4] << setw(8) << pi[5] << setw(8) << pi[6] << setw(8) << pi[7] << setw(8) << pi[8] << setw(8) << Mean << "+/-" << setw(11) << StdDev << setw(12) << abs(Mean/M_PI-1.) << "+/-" << setw(11) << abs(StdDev/M_PI) << setw(14) << flush;
+			if(Mean-StdDev < M_PI && Mean+StdDev > M_PI)
+				cout << "Success" << endl;
+			else if(!isnan(StdDev))
+				cout << "Fail" << endl;
+			else
+				cout << "Indeterminate" << endl;
+		}
+	}while(true);
 
 	return(0);
 }
