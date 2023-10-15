e38382e6db3b883578863b695e13a0fd5ab041f3
Yitzack <isarver@physics.tamu.edu>
2023-10-14 23:43:49 -0600

    Added probability distribution functions and a few features to the Cauchy Distribution object.

--- Lorentz_Monte_Carlo/Importance_Monte_Carlo.cpp
+++ Lorentz_Monte_Carlo/Importance_Monte_Carlo.cpp
@@ -45,6 +45,7 @@ template <class RealType = double> class Cauchy
 		Cauchy(const param_type<RealType>& parm);
 		Cauchy(RealType x0 = 0, RealType gamma = 1);
 		RealType operator()(RealType p);
+		RealType operator()(RealType p, const param_type<RealType>& parm);
 		void reset(){return;};						//Reset the distribution. Has no effect as values are independent of each other.
 		void param(const param_type<RealType>& parm);			//set parameters to those given
 		param_type<RealType> param() const{return(Parameters);}	//return the current parameters
@@ -52,7 +53,8 @@ template <class RealType = double> class Cauchy
 		RealType max(){return(numeric_limits<RealType>::infinity());}
 		RealType a() const{return(Parameters.x0);}
 		RealType b() const{return(Parameters.gamma);}
-		RealType pdf(RealType x){return(1./(M_PI*Parameters.gamma*(1.+pow((x-Parameters.x0)/Parameters.gamma,2))));}
+		RealType pdf(RealType x);	//Probability Distribution Function
+		RealType pdf(RealType x, const param_type<RealType>& parm);
 };
 
 template <class RealType> Cauchy<RealType>::Cauchy(const param_type<RealType>& parm)
@@ -74,6 +76,13 @@ template <class RealType> RealType Cauchy<RealType>::operator()(RealType p)
 	return(std::numeric_limits<RealType>::quiet_NaN());
 }
 
+template <class RealType> RealType Cauchy<RealType>::operator()(RealType p, const param_type<RealType>& parm)
+{
+	if(0 <= p && p <= 1)
+		return(parm.x0+parm.gamma*tan(M_PI*(p-.5)));
+	return(std::numeric_limits<RealType>::quiet_NaN());
+}
+
 template <class RealType> void Cauchy<RealType>::param(const param_type<RealType>& parm)
 {
 	Parameters.x0 = parm.x0;
@@ -81,10 +90,52 @@ template <class RealType> void Cauchy<RealType>::param(const param_type<RealType
 	return;
 }
 
+template <class RealType> RealType Cauchy<RealType>::pdf(RealType x)
+{
+	return(1./(M_PI*Parameters.gamma*(1.+pow((x-Parameters.x0)/Parameters.gamma,2))));
+}
+
+template <class RealType> RealType Cauchy<RealType>::pdf(RealType x, const param_type<RealType>& parm)
+{
+	return(1./(M_PI*parm.gamma*(1.+pow((x-parm.x0)/parm.gamma,2))));
+}
+
+vector<double> Lorentz_Point_Location(vector<double> UR, vector<double> x0, double gamma)
+{
+	double PDF = 1;
+	Cauchy<double> Distro;
+	param_type<double> Para;
+	Para.gamma = gamma;
+	vector<double>::iterator it_UR = UR.begin();
+	for(vector<double>::iterator it_x0 = x0.begin(); it_x0 != x0.end(); it_x0++)
+	{
+		Para.x0 = *it_x0;
+		*it_UR *= Distro(*it_UR, Para);
+		it_UR++;
+	}
+	return(UR);
+}
+
+double Lorentz_Point_PDF(vector<double> x, vector<double> x0, double gamma)
+{
+	double PDF = 1;
+	Cauchy<double> Distro;
+	param_type<double> Para;
+	Para.gamma = gamma;
+	vector<double>::iterator it_x0 = x0.begin();
+	for(vector<double>::iterator it_x = x.begin(); it_x != x.end(); it_x++)
+	{
+		Para.x0 = *it_x0;
+		PDF *= Distro.pdf(*it_x, Para);
+		it_x0++;
+	}
+	return(PDF);
+}
+
 double Lorentz_Point(double A, vector<double> x, vector<double> x0, double gamma)
 {
 	double Denominator = 0;
-	vector<double>:: iterator it_x0 = x0.begin();
+	vector<double>::iterator it_x0 = x0.begin();
 	for(vector<double>::iterator it_x = x.begin(); it_x != x.end(); it_x++)
 	{
 		if(abs(*it_x) >= 1)
@@ -115,6 +166,14 @@ double f1(vector<double> x)	//int_-1^1 dx int_-1^1 dy f1(x,y) = .966673912888639
 	return(Lorentz_Point(A, x, x0, gamma));
 }
 
+double f1_PDF(vector<double> UR)
+{
+	vector<double> x0 = {0,0};
+	double gamma = .1;
+	vector<double> x = Lorentz_Point_Location(UR, x0, gamma);
+	return(Lorentz_Point_PDF(x, x0, gamma));
+}
+
 double f2(vector<double> x)	//int_-1^1 dx int_-1^1 dy f2(x,y) = .1809247246180574
 {
 	vector<double> x0 = {0,0};
@@ -133,12 +192,41 @@ double f3(vector<double> x)	//int_-1^1 dx int_-1^1 dy f3(x,y) = 1.84282940208542
 	return(Lorentz_Point(A, x, x0, gamma)+Lorentz_Point(A, x, x1, gamma));
 }
 
+double f23_PDF(vector<double> UR)
+{
+	vector<double> x0 = {0,0};
+	vector<double> x1 = {.5,.5};
+	double gamma = .1;
+	vector<double> x;
+	if(UR[2] < .5)
+		x = Lorentz_Point_Location(UR, x0, gamma);
+	else
+		x = Lorentz_Point_Location(UR, x1, gamma);
+	double PDF = Lorentz_Point_PDF(x, x0, gamma)/2.;
+	PDF += Lorentz_Point_PDF(x, x1, gamma)/2.;
+	return(PDF);
+}
+
 double f4(vector<double> x)	//int_-1^1 dx int_-1^1 dy f4(x,y) = 3.491194554007413
 {
 	double x0 = 0;
 	double gamma = .1;
 	double A = 2;
 	return(Lorentz_Surface(A, x, x0, gamma));
+}
+
+double f4_PDF(vector<double> UR)
+{
+	double x0 = 0;
+	vector<double> x;
+	Cauchy<double> Distro;
+	param_type<double> Para;
+	Para.gamma = .1;
+	x.resize(2);
+	x[0] = 2.*UR[0]-1.;
+	Para.x0 = -x[0];
+	x[1] = Distro(UR[1], Para);
+	return(Distro.pdf(x[1], Para));
 }
 
 double f5(vector<double> x)	//int_-1^1 dx int_-1^1 dy f5(x,y) = 1.839868331507421
@@ -165,12 +253,38 @@ double f6(vector<double> x)	//int_-1^1 dx int_-1^1 dy f6(x,y) = 6.31956949113983
 	return(f1+f2);
 }
 
+double f56_PDF(vector<double> UR)
+{
+	double x0 = 0;
+	vector<double> x;
+	Cauchy<double> Distro;
+	param_type<double> Para;
+	Para.gamma = .1;
+	x.resize(2);
+	x[0] = 2.*UR[0]-1.;
+	if(UR[2] < .5)
+		Para.x0 = -x[0];
+	else
+		Para.x0 = x[0]-.5;
+	x[1] = Distro(UR[1], Para);
+	return(Distro.pdf(x[1], Para));
+}
+
 double f7(vector<double> x)	//int_-1^1 dx f7(x) = A/pi*(ArcTan((b-x0)/gamma)+ArcTan((x0-a)/gamma)) = (atan(7.5)+atan(12.5))/M_PI = .932397
 {
 	double A = 1;
 	double gamma = .1;
 	double x0 = -.25;
 	return(Lorentz_Surface(A, x, x0, gamma));
+}
+
+double f7_PDF(vector<double> UR)
+{
+	vector<double> x;
+	Cauchy<double> Distro(-.25,.1);
+	x.resize(1);
+	x[0] = Distro(UR[1]);
+	return(Distro.pdf(x[1]));
 }
 
 double f8(vector<double> x)	//int_{unit 10-cube} dV f8(vec x) = 5.6356 (Multi-dimensional), 5.63568+/-.0254724 (Monte Carlo), 5.6357+/-.0254821 (Adaptive Monte Carlo), 5.64082 (Adaptive Quasi Monte Carlo), 5.63616 (Quasi Monte Carlo)
@@ -181,6 +295,14 @@ double f8(vector<double> x)	//int_{unit 10-cube} dV f8(vec x) = 5.6356 (Multi-di
 	return(Lorentz_Point(A, x, x0, gamma));
 }
 
+double f8_PDF(vector<double> UR)
+{
+	vector<double> x0 = {.06,.25,.18,.79,-.05,-.08,.86,.52,.83,.97};
+	double gamma = .1;
+	vector<double> x = Lorentz_Point_Location(UR, x0, gamma);
+	return(Lorentz_Point_PDF(x, x0, gamma));
+}
+
 int main(int argc, char* argv[])
 {
 	unsigned long long int* Samples_Used;
@@ -189,7 +311,7 @@ int main(int argc, char* argv[])
 	double* Mean;
 	double* StdDev;
 	double Correct;
-	int Dims;
+	int Dims, URDims;
 	double (*f)(vector<double>);
 
 	if(argc == 2)
@@ -197,42 +319,46 @@ int main(int argc, char* argv[])
 		switch(argv[1][0])
 		{
 			case '1':
-				Dims = 2;
+				URDims = Dims = 2;
 				f = f1;
 				Correct = .966673912888639;
 				break;
 			case '2':
 				Dims = 2;
+				URDims = 3;
 				f = f2;
 				Correct = .1809247246180574;
 				break;
 			case '3':
 				Dims = 2;
+				URDims = 3;
 				f = f3;
 				Correct = 1.842829402085427;
 				break;
 			case '4':
-				Dims = 2;
+				URDims = Dims = 2;
 				f = f4;
 				Correct = 3.491194554007413;
 				break;
 			case '5':
 				Dims = 2;
+				URDims = 3;
 				f = f5;
 				Correct = 1.839868331507421;
 				break;
 			case '6':
 				Dims = 2;
+				URDims = 3;
 				f = f6;
 				Correct = 6.319569491139836;
 				break;
 			case '7':
-				Dims = 1;
+				URDims = Dims = 1;
 				f = f7;
 				Correct = (atan(7.5)+atan(12.5))/M_PI;
 				break;
 			case '8':
-				Dims = 10;
+				URDims = Dims = 10;
 				f = f8;
 				Correct = 5.6356;
 				break;
@@ -248,7 +374,7 @@ int main(int argc, char* argv[])
 	}
 
 	mt19937* RNG;
-	uniform_real_distribution<double> Archetype(-1.,1.);
+	uniform_real_distribution<double> Archetype(0.,1.);
 	uniform_real_distribution<double>* Uniform;
 	cout << setprecision(18);
 
@@ -277,9 +403,11 @@ int main(int argc, char* argv[])
 	do
 	{
 		vector<double> x;
+		vector<double> URV;
 		x.resize(Dims);
-		for(int i = 0; i < Dims; i++)
-			x[i] = Uniform[omp_get_thread_num()](RNG[omp_get_thread_num()]);
+		URV.resize(URDims);
+		for(int i = 0; i < URDims; i++)
+			URV[i] = Uniform[omp_get_thread_num()](RNG[omp_get_thread_num()]);
 		double Sample = f(x);
 
 		First_Moment[omp_get_thread_num()] = double(Samples_Used[omp_get_thread_num()])/(double(Samples_Used[omp_get_thread_num()]+1))*First_Moment[omp_get_thread_num()]+1./(double(Samples_Used[omp_get_thread_num()]+1))*Sample;
