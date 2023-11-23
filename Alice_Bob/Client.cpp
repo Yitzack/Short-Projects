cc34fd1c556d48d6261deb77bdfad86900434248
Yitzack <isarver@physics.tamu.edu>
2023-11-23 14:13:27 -0700

    Minor changes
    Uncomment the core functionality of Server
    Change strings and char arrays to uint_8 arrays
    Format changes to README

--- Alice_Bob/Client.cpp
+++ Alice_Bob/Client.cpp
@@ -19,12 +19,12 @@ int main()
 	tcp::socket socket(io);	//Creat a socket in the I/O Context
 	socket.connect(tcp::endpoint(ip::address::from_string("127.0.0.1"), 50624));	//Connect the socket to the server at 127.0.0.1:50624
 
-	char buf[1024];	//I don't know why a chacter array (either the primary datatype or std) has to be used to receive but a string can used to send.
+	uint8_t buf[1024];	//I don't know why a chacter array (either the primary datatype or std) has to be used to receive but a string can used to send.
 	boost::system::error_code error;	//Error Code that should be used in sending and receiving data. The error code can't be used unless a flag is also set.
 	socket.receive(buffer(buf,1024));	//Receive into the character array, a data stream from the socket and store it in the buffer.
 	cout << "Received: " << buf << endl;
 
-	string message = "Hello, server!";	//Send a message back to the server
+	uint8_t message[] = "Hello, server!";	//Send a message back to the server
 	socket.send(buffer(message));
 
 	return(0);
--- Alice_Bob/Server.cpp
+++ Alice_Bob/Server.cpp
@@ -19,7 +19,7 @@ SHA256 Hashing;
 int main()
 {
 	Initalize_RSA();
-	/*io_service io;	//I/O Context
+	io_service io;	//I/O Context
 	tcp::acceptor acceptor(io, tcp::endpoint(tcp::v4(), 50624));	//Create a connection acceptor into the I/O context
 
 	//while(true)
@@ -27,11 +27,11 @@ int main()
 		tcp::socket socket(io);	//Creat a socket in the I/O Context
 		acceptor.accept(socket);	//Accept a connection on the socket, passively wait for a connection
 
-		string message = "Hello, client!";	//Before the client can send a message, send it a hello, it is looking for the hello
+		uint8_t message[] = "Hello, client!";		//Before the client can send a message, send it a hello, it is looking for the hello
 		boost::system::error_code ignored_error;	//Error Code that should be used in sending and receiving data. The error code can't be used unless a flag is also set.
 		socket.send(buffer(message));	//Send the message
 
-		char buf[1024];	//I don't know why a chacter array (either the primary datatype or std) has to be used to receive but a string can used to send.
+		uint8_t buf[1024];	//I don't know why a chacter array (either the primary datatype or std) has to be used to receive but a string can used to send.
 		socket.receive(buffer(buf,1024));	//Receive the reply
 		cout << "Received: " << buf << endl;
 
@@ -39,7 +39,7 @@ int main()
 		tcp::endpoint remote_endpoint = socket.remote_endpoint();	//Record the socket remote endpoint
 		string client_ip = remote_endpoint.address().to_string();	//Pull out the IP address of the remote endpoint
 		cout << "Message received from IP address: " << client_ip << endl;	//Log the IP address
-	}*/
+	}
 
 	return(0);
 }
