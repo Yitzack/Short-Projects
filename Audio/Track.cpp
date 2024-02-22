fdaef32b562b25b06ec650fe421e5eab30e0f0b1
Yitzack <isarver@physics.tamu.edu>
2024-02-21 23:40:25 -0700

    Writing down some planned functions so they aren't forgotten

--- Audio/Track.cpp
+++ Audio/Track.cpp
@@ -145,3 +145,204 @@ void Track::Triangle(float duration, Track frequency, Track amplitude, float DC)
 		Data.push_back(amplitude[i]*Triangle(float(i)*frequency[i]*2.*M_PI/float(SAMPLE_RATE))+DC);
 }
 
+void Track::Noise(float duration)
+{
+}
+
+void Track::ADSR(float start, float attack, float decay, float sustain, float sustain_level, float release)
+{
+	int i = start*SAMPLE_RATE;
+	for(i; i < (start+attack)*SAMPLE_RATE; i++)
+		Data[i] *= (i-start*SAMPLE_RATE)/(attack*SAMPLE_RATE);
+	for(i; i < (start+attack+decay)*SAMPLE_RATE; i++)
+		Data[i] *= (sustain_level-1.)/(decay*SAMPLE_RATE)*(i-(start+attack)*SAMPLE_RATE)+1.;
+	for(i; i < (start+attack+decay+sustain)*SAMPLE_RATE; i++)
+		Data[i] *= sustain_level;
+	for(i; i < (start+attack+decay+sustain+decay)*SAMPLE_RATE; i++)
+		Data[i] *= -sustain_level/(release*SAMPLE_RATE)*(i-(start+attack+decay+sustain)*SAMPLE_RATE)+sustain_level;
+}
+
+void Track::Compressor(float ratio, float threashold, float attack, float sustain, float release)
+{
+}
+
+void Track::High_Cut(float Threashold, float Roll_off)
+{
+}
+
+void Track::High_Cut(Track Threashold, float Roll_off)
+{
+}
+
+void Track::High_Cut(float Threashold, Track Roll_off)
+{
+}
+
+void Track::High_Cut(Track Threashold, Track Roll_off)
+{
+}
+
+void Track::Low_Cut(float Threashold, float Roll_off)
+{
+}
+
+void Track::Low_Cut(Track Threashold, float Roll_off)
+{
+}
+
+void Track::Low_Cut(float Threashold, Track Roll_off)
+{
+}
+
+void Track::Low_Cut(Track Threashold, Track Roll_off)
+{
+}
+
+void Track::High_Shelf(float Threashold, float Roll_off, float Amp)
+{
+}
+
+void Track::High_Shelf(Track Threashold, float Roll_off, float Amp)
+{
+}
+
+void Track::High_Shelf(float Threashold, Track Roll_off, float Amp)
+{
+}
+
+void Track::High_Shelf(float Threashold, float Roll_off, Track Amp)
+{
+}
+
+void Track::High_Shelf(float Threashold, Track Roll_off, Track Amp)
+{
+}
+
+void Track::High_Shelf(float Threashold, float Roll_off, Track Amp)
+{
+}
+
+void Track::High_Shelf(Track Threashold, float Roll_off, float Amp)
+{
+}
+
+void Track::High_Shelf(Track Threashold, Track Roll_off, float Amp)
+{
+}
+
+void Track::High_Shelf(Track Threashold, Track Roll_off, Track Amp)
+{
+}
+
+void Track::Low_Shelf(float Threashold, float Roll_off, float Amp)
+{
+}
+
+void Track::Low_Shelf(Track Threashold, float Roll_off, float Amp)
+{
+}
+
+void Track::Low_Shelf(float Threashold, Track Roll_off, float Amp)
+{
+}
+
+void Track::Low_Shelf(float Threashold, float Roll_off, Track Amp)
+{
+}
+
+void Track::Low_Shelf(float Threashold, Track Roll_off, Track Amp)
+{
+}
+
+void Track::Low_Shelf(float Threashold, float Roll_off, Track Amp)
+{
+}
+
+void Track::Low_Shelf(Track Threashold, float Roll_off, float Amp)
+{
+}
+
+void Track::Low_Shelf(Track Threashold, Track Roll_off, float Amp)
+{
+}
+
+void Track::Low_Shelf(Track Threashold, Track Roll_off, Track Amp)
+{
+}
+
+void Track::Band_Amp(float Frequency, float Width, float Amp)
+{
+}
+
+void Track::Band_Amp(Track Frequency, float Width, float Amp)
+{
+}
+
+void Track::Band_Amp(float Frequency, Track Width, float Amp)
+{
+}
+
+void Track::Band_Amp(float Frequency, float Width, Track Amp)
+{
+}
+
+void Track::Band_Amp(float Frequency, Track Width, Track Amp)
+{
+}
+
+void Track::Band_Amp(float Frequency, float Width, Track Amp)
+{
+}
+
+void Track::Band_Amp(Track Frequency, float Width, float Amp)
+{
+}
+
+void Track::Band_Amp(Track Frequency, Track Width, float Amp)
+{
+}
+
+void Track::Band_Amp(Track Frequency, Track Width, Track Amp)
+{
+}
+
+void Track::Band_Pass(float Frequency, float Roll_off)
+{
+}
+
+void Track::Band_Pass(Track Frequency, float Roll_off)
+{
+}
+
+void Track::Band_Pass(float Frequency, Track Roll_off)
+{
+}
+
+void Track::Band_Pass(Track Frequency, Track Roll_off)
+{
+}
+
+void Track::Fader(float Amp)
+{
+}
+
+void Track::Fader(Track Amp)
+{
+}
+
+Track Track::operator*(float Amp)
+{
+}
+
+Track Track::operator+(Track Other)
+{
+}
+
+Track Track::concat(Track Other)
+{
+}
+
+Track Track::concat(float Silence_duration, Track Other)
+{
+}
+
--- Audio/Track.h
+++ Audio/Track.h
@@ -14,6 +14,8 @@ class Track
 {
 	public:
 	Track(){};
+
+	//Simple function generators and FM synthesis
 	void Sine(float, float, float, float);
 	void Sine(float, Track, float, float);
 	void Sine(float, float, Track, float);
@@ -32,11 +34,77 @@ class Track
 	void Triangle(float, float, Track, float);
 	void Triangle(float, Track, Track, float);
 	float Triangle(float theta){ return(1.-2.*abs(2.*((theta-M_PI/2.)/(2.*M_PI)-floor(.5+(theta-M_PI/2.)/(2.*M_PI))))); }
+	void Noise(float);
+
+	//Sound shaping functions
+	void ADSR(float, float, float, float, float, float);
+	void Compressor(float, float, float, float, float);
+	void Gate();
+	void Duck();
+	void Low_Cut(float, float);
+	void Low_Cut(Track, float);
+	void Low_Cut(float, Track);
+	void Low_Cut(Track, Track);
+	void High_cut(float, float);
+	void High_cut(Track, float);
+	void High_cut(float, Track);
+	void High_cut(Track, Track);
+	void High_Shelf(float, float, float);
+	void High_Shelf(Track, float, float);
+	void High_Shelf(float, Track, float);
+	void High_Shelf(float, float, Track);
+	void High_Shelf(float, Track, Track);
+	void High_Shelf(float, float, Track);
+	void High_Shelf(Track, float, float);
+	void High_Shelf(Track, Track, float);
+	void High_Shelf(Track, Track, Track);
+	void Low_Shelf(float, float, float);
+	void Low_Shelf(Track, float, float);
+	void Low_Shelf(float, Track, float);
+	void Low_Shelf(float, float, Track);
+	void Low_Shelf(float, Track, Track);
+	void Low_Shelf(float, float, Track);
+	void Low_Shelf(Track, float, float);
+	void Low_Shelf(Track, Track, float);
+	void Low_Shelf(Track, Track, Track);
+	void Band_Amp(float, float, float);
+	void Band_Amp(Track, float, float);
+	void Band_Amp(float, Track, float);
+	void Band_Amp(float, float, Track);
+	void Band_Amp(float, Track, Track);
+	void Band_Amp(float, float, Track);
+	void Band_Amp(Track, float, float);
+	void Band_Amp(Track, Track, float);
+	void Band_Amp(Track, Track, Track);
+	void Band_Pass(float, float);
+	void Band_Pass(Track, float);
+	void Band_Pass(float, Track);
+	void Band_Pass(Track, Track);
+
+	//Effects
+	void Delay(float, float);
+	void Delay(Track, float);
+	void Delay(float, Track);
+	void Delay(Track, Track);
+	void Reverb(float, float, float, float, float, float, float, float, float);
+	void Plate();
+	void Spring();
+
+	//Mixing functions
+	void Fader(float);
+	void Fader(Track);
+	Track operator*(float);
+	Track operator+(Track);
+	Track concat(Track);
+	Track concat(float, Track);
+
+	//Various helper functions
 	int Length(){ return(Data.size()); }
 	int16_t operator[](int i){ return(Data[i]); }
 	float Sign(float x){ return(x==0?0:x/abs(x)); }
 	double Sign(double x){ return(x==0?0:x/abs(x)); }
 	int16_t Sign(int16_t x){ return(x==0?0:x/abs(x)); }
+
 	private:
 	vector<int16_t> Data;
 };
